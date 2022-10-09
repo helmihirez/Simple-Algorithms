88ed34389588f99a1e7e89449014beae1af6f2e0
Helmi Hirez <112577171+helmihirez@users.noreply.github.com>
2022-10-09 18:47:05 +0300

    Update Multiplication_Table.dart

--- Multiplication_Table.dart
+++ Multiplication_Table.dart
@@ -6,7 +6,7 @@ Steps:
   2-Let num=1, count=0
   3-If num=13 go step 7
   4-If count != 10 Let count=count+1 else let num=num+1 and count=1
-  5-print “the result of” num”*” count = num*count
+  5-print “the result of” num ”*” count "=" num*count
   6-repeat 3,4,5
   7-End
 */
