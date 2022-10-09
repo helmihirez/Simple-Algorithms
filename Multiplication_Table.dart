242becc3106937ecec67d877b2837907471fed71
Helmi Hirez <helmihirez@gmail.com>
2022-10-09 17:18:13 +0300

    Multiplication Tables
    Writing an algorithm that prints out the multiplication table for numbers up to 12

---
+++ Multiplication_Table.dart
@@ -0,0 +1,29 @@
+/*
+Q: Write an algorithm that prints out the multiplication table for numbers up to 12.
+
+Steps:
+  1-Start
+  2-Let num=1, count=0
+  3-If num=13 go step 7
+  4-If count != 10 Let count=count+1 else let num=num+1 and count=1
+  5-print “the result of” num”*” count = num*count
+  6-repeat 3,4,5
+  7-End
+*/
+main() {
+  var num = 1, count = 1;
+  print("--- The $num Table ---");
+  while (num <= 12) {
+    if (count <= 10) {
+      print("$num * $count = ${num * count}");
+      count += 1;
+    } else {
+      count = 1;
+      num += 1;
+      if (num != 13) {
+        print("--- The $num Table ---");
+      }
+    }
+  }
+  print("Done!");
+}
